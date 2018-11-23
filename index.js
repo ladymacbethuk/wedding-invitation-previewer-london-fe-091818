57a662fdcc17a7626b6d0ae929c95f95bcc6f93e
Stephanie Phillips <steph@ladymacbeth.uk>
2018-11-23 09:32:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function formEntry(event) {
+  let a = document.getElementById("name1").value;
+  a.innerHTML;
+  document.getElementById("n1").innerHTML = a;
+  let b = document.getElementById("name2").value;
+  b.innerHTML;
+  document.getElementById("n2").innerHTML = b;
+  let c = document.getElementById("nameL").value;
+  c.innerHTML;
+  document.getElementById("la").innerHTML = c;
+  let d = document.getElementById("date").value;
+  d.innerHTML;
+  document.getElementById("da").innerHTML = d;
+  let l = document.getElementById("location").value;
+  l.innerHTML;
+  document.getElementById("lo").innerHTML = l;
+   }
+   
+ function styleFunction1() {
+   document.getElementById("sand").style.color = "#977316";
+   document.getElementById("lights").style.color = "#DAA520";
+   document.getElementById("book").style.color = "#DAA520";
+   document.getElementById('invite').className = 'style1';
+   document.getElementById("wed").style.color = "#DAA520";
+ } 
+
+ function styleFunction2() {
+   document.getElementById("lights").style.color = "#977316";
+   document.getElementById("sand").style.color = "#DAA520";
+   document.getElementById("book").style.color = "#DAA520";
+   document.getElementById('invite').className = 'style2';
+   document.getElementById("wed").style.color = "#FF0080";
+ } 
+  function styleFunction3() {
+    document.getElementById("book").style.color = "#977316";
+    document.getElementById("lights").style.color = "#DAA520";
+   document.getElementById("sand").style.color = "#DAA520";
+   document.getElementById('invite').className = 'style3';
+   document.getElementById("wed").style.color = "#BA55D3";
+ } 
+ 
